8d4bda7afc9bfcbe896723e8635df57cb3d7e86a
Chengming <v-cruan@microsoft.com>
2022-12-12 15:04:18 +0800

    update

---
+++ src/ProgrammerToolkitBackend/ProgrammerToolkitBackend/Controllers/RandomToolsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammerToolkitBackend.Controllers
+{
+    [ApiController]
+    public class RandomToolsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/RandonTools/Ipv4/{count}")]
+        public List<string> GetRandomIpv4Address(int count = 1)
+        {
+            List<string> list = new List<string>();
+            // todo
+            return list;
+        }
+
+        [HttpGet]
+        [Route("api/RandonTools/Ipv6/{count}")]
+        public List<string> GetRandomIpv6Address(int count = 1)
+        {
+            List<string> list = new List<string>();
+            // todo
+            return list;
+        }
+
+        [HttpGet]
+        [Route("api/RandonTools/Password/{count}")]
+        public List<string> GetRandomPassword(int count = 1)
+        {
+            List<string> list = new List<string>();
+            // todo
+            return list;
+        }
+    }
+}
