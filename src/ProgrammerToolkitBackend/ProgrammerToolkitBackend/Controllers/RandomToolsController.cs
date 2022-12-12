16ab92b6a8340c704a01549b460b004c61ffaacf
Chengming <61542202+dvbb@users.noreply.github.com>
2022-12-12 22:07:29 +0800

    Random tools init (#1)
    * update
    
    * update

---
+++ src/ProgrammerToolkitBackend/ProgrammerToolkitBackend/Controllers/RandomToolsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammerToolkit.Core;
+using ProgrammerToolkitBackend.Provider;
+
+namespace ProgrammerToolkitBackend.Controllers
+{
+    [ApiController]
+    public class RandomToolsController : ControllerBase
+    {
+        public RandomToolsProvider _randomToolsProvider => new RandomToolsProvider();
+
+        [HttpGet]
+        [Route("api/RandonTools/Ipv4/{count}")]
+        public List<string> GetRandomIpv4Address(int count = 1)
+        {
+            ParameterValidator.ValidatePositiveNumber(count);
+            List<string> list = _randomToolsProvider.GetRandomIpv4Addresses(count);
+            return list;
+        }
+
+        [HttpGet]
+        [Route("api/RandonTools/Ipv6/{count}")]
+        public List<string> GetRandomIpv6Address(int count = 1)
+        {
+            ParameterValidator.ValidatePositiveNumber(count);
+            List<string> list = new List<string>();
+            // todo
+            return list;
+        }
+
+        [HttpGet]
+        [Route("api/RandonTools/Password/{count}")]
+        public List<string> GetRandomPassword(int count = 1)
+        {
+            ParameterValidator.ValidatePositiveNumber(count);
+            List<string> list = new List<string>();
+            // todo
+            return list;
+        }
+    }
+}
---
+++ src/ProgrammerToolkitBackend/ProgrammerToolkitBackend/Provider/RandomToolsProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace ProgrammerToolkitBackend.Provider
+{
+    public class RandomToolsProvider
+    {
+        private readonly Random _random = new Random();
+
+        public List<string> GetRandomIpv4Addresses(int count)
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(GetRandomIpv4Address());
+            }
+            return list;
+        }
+
+        private string GetRandomIpv4Address() => $"{_random.Next(255)}.{_random.Next(255)}.{_random.Next(255)}.{_random.Next(255)}";
+    }
+}
