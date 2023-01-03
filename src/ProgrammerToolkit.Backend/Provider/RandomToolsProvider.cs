cae9106a8103e91d1c35ff648a35b0be8af50827
Chengming <v-cruan@microsoft.com>
2023-01-03 16:51:06 +0800

    sync previous code

---
+++ src/ProgrammerToolkit.Backend/Provider/RandomToolsProvider.cs
@@ -0,0 +1,52 @@
+﻿namespace ProgrammerToolkitBackend.Provider
+{
+    public class RandomToolsProvider
+    {
+        private readonly Random _random = new Random();
+
+        public List<string> GetRandomIpv4Addresses(int count)
+        {
+            var list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(GetRandomIpv4Address());
+            }
+            return list;
+        }
+
+        private string GetRandomIpv4Address() => $"{_random.Next(255)}.{_random.Next(255)}.{_random.Next(255)}.{_random.Next(255)}";
+
+        public List<string> GetRandomIpv6Addresses(int count)
+        {
+            var list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(GetRandomIpv6Address());
+            }
+            return list;
+        }
+
+        private string GetRandomIpv6Address() => $"{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}:{_random.Next(65536).ToString("X")}";
+
+        public List<string> GetRandomPasswords(int count)
+        {
+            var list = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(GetRandomPassword());
+            }
+            return list;
+        }
+
+        private string GetRandomPassword()
+        {
+            int length = _random.Next(8,16);
+            string password = "";
+            for (int i = 0; i < length; i++)
+            {
+                password += (Char)_random.Next(33,125);
+            }
+            return password;
+        }
+    }
+}
---
+++ src/ProgrammerToolkit.Backend/Provider/WebToolsProvider.cs
@@ -0,0 +1,25 @@
+﻿using ProgrammerToolkit.Core.Errors;
+using ProgrammerToolkitBackend.IProvider;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProgrammerToolkitBackend.Provider
+{
+    public class WebToolsProvider: IWebToolsProvider
+    {
+        public async Task<string> GetWebTools(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            try
+            {
+                var claims = handler.ReadToken(token);
+                return claims.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Invalid format.",ex);
+            }
+        }
+    }
+}
