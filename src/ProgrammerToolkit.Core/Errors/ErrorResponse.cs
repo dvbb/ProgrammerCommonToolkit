0ca776016e7e529ef4d5f0478d69d6cc43a44fd5
Chengming <v-cruan@microsoft.com>
2023-01-03 16:46:44 +0800

    reconstruct

---
+++ src/ProgrammerToolkit.Core/Errors/ErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerToolkit.Core.Errors
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse()
+        {
+            Errors=new List<ErrorInfo>();
+        }
+        public ErrorResponse(ErrorInfo errorInfo)
+        {
+            Errors.Add(errorInfo);
+        }
+        public ErrorResponse(List<ErrorInfo> errors)
+        {
+            Errors.AddRange(errors);
+        }
+
+        public List<ErrorInfo> Errors = new List<ErrorInfo>();
+    }
+}
