d3a4a219564b52614ff89e62eb84e2cb4adbfc21
Chengming <v-cruan@microsoft.com>
2023-01-05 18:15:06 +0800

    Update RandomToolsTests.cs

--- src/ProgrammerToolkit.Test/RandomToolsTests.cs
+++ src/ProgrammerToolkit.Test/RandomToolsTests.cs
@@ -5,15 +5,14 @@ namespace ProgrammerToolkit.Test
     public class RandomToolsTests
     {
         private IRandomToolsProvider _randomToolsProvider;
-        
+
+        public RandomToolsTests()
+        {
+        }
+
         public RandomToolsTests(IRandomToolsProvider randomToolsProvider)
         {
             _randomToolsProvider = randomToolsProvider;
-        }
-
-        [SetUp]
-        public void Setup()
-        {
         }
 
         [Test]
