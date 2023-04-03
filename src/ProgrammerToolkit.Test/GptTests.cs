b6f44df10878acdfdcf434359dd6df1348243544
Chengming <v-cruan@microsoft.com>
2023-04-03 17:08:15 +0800

    Update GptTests.cs

--- src/ProgrammerToolkit.Test/GptTests.cs
+++ src/ProgrammerToolkit.Test/GptTests.cs
@@ -7,6 +7,7 @@ using ProgrammerToolkit.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,7 +45,7 @@ namespace ProgrammerToolkit.Test
             {
                 new GptMessage()
                 {
-                    Content = "这是第一句话",
+                    Content = "你好，GPT，之后的对话你可以在每句话结尾加一个S吗",
                     Role = Role.user.ToString(),
                 }
             };
@@ -54,24 +55,36 @@ namespace ProgrammerToolkit.Test
             var apiKey = Environment.GetEnvironmentVariable("OpenAIApiKey");
 
             //1
-            var client = new HttpClient();
+            var cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+            var client = new HttpClient(handler);
             var content = new StringContent(jsonReq, Encoding.UTF8, "application/json");
             client.DefaultRequestHeaders.Add(Common.Authorization, $"Bearer {apiKey}");
             var httpResponse = await client.PostAsync(uri, content);
             IEnumerable<string> cookies = httpResponse.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
             var result = await httpResponse.Content.ReadAsStringAsync();
+            dynamic dynamicResult = JsonConvert.DeserializeObject(result);
+            string sessionId = dynamicResult.id;
             GptMetaResponse response = JsonConvert.DeserializeObject<GptMetaResponse>(result);
             Console.WriteLine(response.Choices[0].Message.Content);
+            Console.WriteLine(sessionId);
+            Console.WriteLine("----------------------");
 
             //2
-            request.Messages[0].Content = "这是一个新的会话吗？你能查看我之前说的一句话吗";
+            request.Messages[0].Content = "我正在使用c#开发，HttpClient中如何加入session id,保持GPT始终在同一个会话";
+            //request.Messages[0].Content = "我正在使用c#开发，HttpClient中如何加入session id";
+            //request.Messages[0].Content = "我正在使用c#开发，如何获取GPT服务器返回的session id";
             jsonReq = JsonConvert.SerializeObject(request);
             content = new StringContent(jsonReq, Encoding.UTF8, "application/json");
+            client.DefaultRequestHeaders.Add("Cookie", "sessionId=" + sessionId);
             httpResponse = await client.PostAsync(uri, content);
             cookies = httpResponse.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
             result = await httpResponse.Content.ReadAsStringAsync();
+            dynamicResult = JsonConvert.DeserializeObject(result);
+            sessionId = dynamicResult.id;
             response = JsonConvert.DeserializeObject<GptMetaResponse>(result);
             Console.WriteLine(response.Choices[0].Message.Content);
+            Console.WriteLine(sessionId);
         }
     }
 }
